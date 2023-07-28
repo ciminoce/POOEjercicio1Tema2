0184924b134fcdf2a97e32b2f48173e5ea84aa20
ciminoce <ciminoce@yahoo.com.ar>
2023-07-28 13:53:46 -0300

    Agregar archivos de proyecto.

---
+++ POOEjercicio1Tema2.Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿using POOEjercicio1Tema2.Entidades;
+
+namespace POOEjercicio1Tema2.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Conus!");
+            Console.WriteLine("ingrese el valor del radio ");
+            int radio = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el valor de la altura ");
+            int altura = int.Parse(Console.ReadLine());
+
+
+            Cono c = new Cono(altura, radio);
+
+            Console.WriteLine(c.MostrarDatos());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ POOEjercicio1Tema2.Entidades/Cono.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace POOEjercicio1Tema2.Entidades
+{
+    public class Cono
+    {
+        private int _altura;
+        private int _radio;
+        private double _generatriz;
+
+        public double GetGeneratriz() => _generatriz;
+        public int GetAltura() => _altura;
+        public int GetRadio() => _radio;
+        
+
+        public Cono(int altura, int radio)
+        {
+            if (altura <= 0 || radio <= 0)
+            {
+                throw new ArgumentException("Valores mal ingresados");
+            }
+
+            _altura = altura;
+
+
+            _radio = radio;
+            _generatriz = SetGeneratriz();
+        }
+
+        private double SetGeneratriz()
+        {
+            return Math.Sqrt(Math.Pow(GetAltura(), 2) + Math.Pow(GetRadio(), 2));
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * GetRadio() * (GetRadio() + GetGeneratriz());
+        }
+
+        public double GetVolumen()
+        {
+            return Math.PI * Math.Pow(GetRadio(), 2) * (GetAltura() / 3);
+
+        }
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Cono");
+            sb.AppendLine($"Radio....: {GetRadio()}");
+            sb.AppendLine($"Altura...: {GetAltura()}");
+            sb.AppendLine($"Generatríz...: {GetGeneratriz():N}");
+            sb.AppendLine($"Superficie: {GetArea():N}");
+            sb.AppendLine($"Volumen...: {GetVolumen():N}");
+            return sb.ToString();
+        }
+
+    }
+}
